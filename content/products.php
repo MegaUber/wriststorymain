19a929060a4866f76916b37ca1a3bb6ac983dbb2
Glen Murray <gmurray.neonsky@gmail.com>
2016-08-24 16:19:28 -0500

    First Changes
    First Changes

---
+++ content/products.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Wrist Story Fundraising Bracelets</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta property="og:title" content="Bracelets For Fundraising | Custom Made Bracelets | Awareness Wristbands - Wrist Story Products" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Use this page to contact us by phone or through our online form. We look forward to working with you to make your custom made bracelets. Contact us today!" />
+    <meta property="og:url" content="http://www.wriststoryproducts.com/pages/contact-us" />
+    <meta property="og:site_name" content="Wrist Story Products" />
+
+    <meta name="description" content="Use this page to contact us by phone or through our online form. We look forward to working with you to make your custom made bracelets. Contact us today!" />
+
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../css/grid.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/camera.css">
+    <link rel="stylesheet" href="../css/owl-carousel.css">
+    <script src="../js/jquery.js"></script>
+    <script src="../js/jquery-migrate-1.2.1.js"></script><!--[if lt IE 9]>
+    <html class="lt-ie9">
+      <div style="clear: both; text-align:center; position: relative;"><a href="http://windows.microsoft.com/en-US/internet-explorer/.."><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    </html>
+    <script src="js/html5shiv.js"></script><![endif]-->
+    <script src="../js/device.min.js"></script>
+  </head>
+  <body>
+    <div class="page">
+      <!--
+      ========================================================
+      							HEADER
+      ========================================================
+
+
+      -->
+      <header>
+        <div class="container">
+          <div class="brand">
+            <a href="../"><img src="../images/wriststory/wriststoryheader.jpg" alt="Wrist Story Products"></img></a>
+          </div>
+          <a href="Free_Samples/"><img src="../images/wriststory/get_free_samples.jpg" class="getfree" alt="Free Samples"> </img></a>
+          <p>One of our representatives will happily fill your needs within 24 hours.<br></p> <p class="urgent">For urgent needs call us at</p>
+          <a href="callto:8005445138" class="fa-phone">800-544-5138</a>
+        </div>
+        <div id="stuck_container" class="stuck_container">
+          <div class="container">
+            <nav class="nav">
+              <ul data-type="navbar" class="sf-menu">
+                <li class="active"><a href="../">Home</a>
+                </li>
+                <li><a href="Products/">Our Products</a>
+                  <ul>
+                    <li><a href="American_Veteran/">American Veteran</a></li>
+                    <li><a href="American_Pride/">American Pride</a></li>
+                    <li><a href="American_Rider_16/">American Rider 16</a></li>
+                    <li><a href="African_American_pride/">African American Pride</a></li>
+                    <li><a href="African_Icon/">African Icon</a></li>
+                    <li><a href="Ten_Commandments/">Ten Commandments</a></li>
+                    <li><a href="4H_Clubs/">4H Clubs</a></li>
+                    <li><a href="Cancer_Awareness/">Cancer Awareness</a></li>
+                    <li><a href="Custom_Fund_Raiser/">Custom Fund Raiser</a></li>
+                  </ul>
+                </li>
+                <li><a href="Mission/">Our Mission</a>
+                </li>
+                <li><a href="Numbers/">Our Numbers</a>
+                </li>
+                <li><a href="Contact/">Contact Us</a>
+                </li>
+                <li><a href="Team/">Our Team</a>
+                </li>
+                <li><a href="Testimonials/">Testimonials</a>
+                </li>
+                <li><a href="Order/">Order Here</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <!--
+      ========================================================
+                                  CONTENT
+      ========================================================
+      -->
+      <main>
+        Blah Blah Products
+      </main>
+
+      <!--
+      ========================================================
+                                  FOOTER
+      ========================================================
+      -->
+      <footer class="site-footer" role="contentinfo">
+      <div class="container">
+        <div class="row bottom30">
+          <div class="grid_4">
+            <h4>Links</h4>
+            <ul role="navigation">
+              <li><a href="search/" title="Search">Search</a></li>
+              <li><a href="privacy-policy/" title="Privacy Policy">Privacy Policy</a></li>
+            </ul>
+          </div>
+          <div class="grid_4">
+            <h4>Categories</h4>
+            <ul role="navigation">
+              <li><a href="American_Veteran/" title"American Veteran">American Veteran</a></li>
+              <li><a href="American_Pride/" title"American Pride">American Pride</a></li>
+              <li><a href="American_Rider_16/" title"American Rider 16">American Rider 16</a></li>
+              <li><a href="African_American_pride/" title"">African American Pride</a></li>
+              <li><a href="African_Icon/" title"African Icon">African Icon</a></li>
+              <li><a href="Ten_Commandments/" title"Ten Commandments">Ten Commandments</a></li>
+              <li><a href="4H_Clubs/" title"4H Clubs">4H Clubs</a></li>
+              <li><a href="Cancer_Awareness/" title"Cancer Awareness">Cancer Awareness</a></li>
+              <li><a href="Custom_Fund_Raiser/" title"Custom Fund Raiser">Custom Fund Raiser</a></li>
+            </ul>
+          </div>
+          <div class="grid_4">
+            <h4>My Account</h4>
+            <ul role="navigation">
+              <li><a href="/account">View Account</a></li>
+              <li><a href="/cart">My Cart</a></li>
+            </ul>
+          </div>
+          <div class="grid_4">
+            <h4>Social Media</h4>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/company/wrist-story-products-inc-" title="Follow us on linkedin" target="_blank"><span class="shopify-social-icon-linkedin"></span></a>
+            </div>
+          </div>
+        </div>
+      </div><!--/.container-->
+      <div class="footer-bottom">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8">
+              <p><small>Copyright &copy; 2016 Wrist Story Products.</small></p>
+            </div>
+            <div class="col-md-4">
+              <div class="payment-types">
+                <p class="text-right-md">
+                  <img width="40" src="../images/wriststory/creditcards/american_express.svg" />
+                  <img width="40" src="../images/wriststory/creditcards/dinners_club.svg" />
+                  <img width="40" src="../images/wriststory/creditcards/discover.svg" />
+                  <img width="40" src="../images/wriststory/creditcards/jcb.svg" />
+                  <img width="40" src="../images/wriststory/creditcards/master_card.svg" />
+                  <img width="40" src="../images/wriststory/creditcards/visa.svg" />
+                </p>
+                <br>
+              </div>
+            </div><!--/.col-->
+          </div><!--/.row-->
+        </div><!--/.container-->
+      </div><!--/.footer-bottom-->
+    </footer><!--/.site-footer-->
+    </div>
+    <script src="../js/script.js"></script>
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  </body>
+</html>
